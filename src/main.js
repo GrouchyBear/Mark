6daccb5b2d8428557a81fb40a6e1fe7fa7c4ed5c
GrouchyBear <benjaminshlack@gmail.com>
2015-11-16 10:02:10 +0000

    More files

--- src/main.js
+++ src/main.js
@@ -1,41 +1,39 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var config = require('../config.json');
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
+// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-if (process.platform === 'win32') {
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
 
-  var installer = require('./squirrel-installer');
-  var SquirrelUpdater = require('./squirrel-updater');
-  squirrelCommand = process.argv[1];
-  installer.handleStartupEvent(app, squirrelCommand);
-
-  //check for updates
-  var updator = new SquirrelUpdater(config.windowsUpdateUrl);
-  updator.checkForUpdates();
-}
-
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
 app.on('ready', function() {
-
+  // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/browser/index.html');
 
+  // and load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // Open the DevTools.
+  mainWindow.openDevTools();
+
+  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-});
-
-app.on('window-all-closed', function() {
-  app.quit();
 });
